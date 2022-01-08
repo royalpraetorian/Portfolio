51f266c4e982ae891d81ec03713e22952fbffea5
Christopher Ellis <royalpraetorian@gmail.com>
2022-01-08 03:29:28 -0700

    Late night minor changes
    Finally happy with the styling on the "books" page.

--- src/app/writingpage/books-tab/books-tab.component.ts
+++ src/app/writingpage/books-tab/books-tab.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-books-tab',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksTabComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -15,6 +16,11 @@ export class BooksTabComponent implements OnInit {
   public openTab(url: string)
   {
     window.open(url, '_blank')?.focus();
+  }
+
+  public routeTo(route: string)
+  {
+    this.router.navigateByUrl(route);
   }
 
 }
